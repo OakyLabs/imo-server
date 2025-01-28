c95d463dc6d08c39f3a676087bb2a6f2fd5ce4be
Oaky Labs <code@oakylabs.com>
2025-01-28 12:20:48 +0100

    feat: add iconsato every single piece of iconable dashboard

--- src/app/pages/back-office/dashboard/system-status.tsx
+++ src/app/pages/back-office/dashboard/system-status.tsx
@@ -1,7 +1,27 @@
 export function SystemStatus(props: { is_on: boolean; last_changed: Date }) {
   return (
     <div className="bg-white p-6 rounded-lg shadow-md">
-      <h2 className="text-xl font-semibold mb-4">System Status</h2>
+      <div class="flex justify-between items-center mb-4">
+        <h2 class={`text-xl font-semibold $`}>System Status</h2>
+        <div
+          class={`${props.is_on ? "bg-orange-100" : "bg-green-100"} rounded-full p-3`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class={`h-6 w-6 ${props.is_on ? "text-orange-800" : "text-gray-800"}`}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+            />
+          </svg>
+        </div>
+      </div>
       <div className="flex justify-between items-center mb-4">
         <span className="text-sm font-medium">Current Status:</span>
         <span
--- src/app/pages/back-office/dashboard/total-checked-items.tsx
+++ src/app/pages/back-office/dashboard/total-checked-items.tsx
@@ -1,9 +1,28 @@
 export function TotalCheckedItems(props: { count: number }) {
   return (
     <div className="bg-white p-6 rounded-lg shadow-md">
-      <h2 className="text-xl font-semibold mb-2">
-        Número total de leilões vistos
-      </h2>
+      <div class="flex justify-between items-center mb-4">
+        <h2 class="text-xl font-semibold text-gray-800">
+          Número total de leilões vistos
+        </h2>
+        <div class="bg-green-100 rounded-full p-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 text-green-600"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
+            />
+          </svg>
+        </div>
+      </div>
+
       <p className="text-4xl font-bold text-green-600">{props.count}</p>
       <p className="text-sm text-gray-600 mt-2">
         Leilões e ofertas processadas
